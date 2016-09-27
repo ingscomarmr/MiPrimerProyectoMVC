f22c58b72c61652f163ab8439a4889006bf34e8c
omunguia <ingscomarmr@gmail.com>
2016-09-27 15:56:11 -0500

    Esqueleto de  proyecto MVC con Boostrap, jQuery, MVC

---
+++ MiPrimerProyectoMVC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimerProyectoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //localhost:xxxx/home/index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //localhost:xxxx/Home/Estudiantes
+        public ActionResult Estudiantes()
+        {
+            return View();
+        }
+    }
+}
