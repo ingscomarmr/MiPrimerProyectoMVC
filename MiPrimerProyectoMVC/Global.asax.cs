0b8634a95230becca0b064aa20ee763b0a368b4d
omunguia <ingscomarmr@gmail.com>
2016-10-24 09:06:18 -0500

    Implementar función de Application_Error errores globales

--- MiPrimerProyectoMVC/Global.asax.cs
+++ MiPrimerProyectoMVC/Global.asax.cs
@@ -14,5 +14,19 @@ namespace MiPrimerProyectoMVC
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+
+        //metodo implementado para manejar los errores a nivel global
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            Response.Clear();
+
+            HttpException httpException = exception as HttpException;
+
+            int error = httpException != null ? httpException.GetHttpCode() : 0;
+
+            Server.ClearError();
+            Response.Redirect(String.Format("~/Error/?error={0}", error, exception.Message));
+        }
     }
 }
---
+++ MiPrimerProyectoMVC/ViewModel/ContactoVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimerProyectoMVC.ViewModel
+{
+    public class ContactoVM
+    {
+        
+        public string Correo { get; set; }
+        public string TituloMsg { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
